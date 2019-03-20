83618b34a7ff7d574f857a610ac8ec49df4e57ed
Patrick McNamara <hello@patrickmcnamara.xyz>
2019-03-20 22:35:54 +0000

    Improve config for Fish
    - Make most things run only once, instead of every at the start of fish instance
    - Set fish greeting to current TTY name

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,28 @@
-### $GOPATH and $PATH
-set -x GOPATH ~/Go
-set -x PATH $GOPATH/bin $PATH
+if status is-login
+    ### $GOPATH and $PATH
+    set -x GOPATH ~/.go
+    set -x PATH $GOPATH/bin/ $PATH
+    set -x PATH ~/.cargo/bin/ $PATH
 
-### Make GnuPG gpg-agent ssh-agent thing work
-set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-set -gx GPG_TTY (tty)
-gpg-connect-agent updatestartuptty /bye > /dev/null
+    ### EDITOR, VISUAL, TERMINAL and PAGER
+    set -gx EDITOR kak
+    set -gx VISUAL kak
+    set -gx TERMINAL alacritty
+    set -gx PAGER less
 
-### EDITOR, VISUAL, TERMINAL and PAGER
-set -gx EDITOR kak
-set -gx VISUAL kak
-set -gx TERMINAL alacritty
-set -gx PAGER less
+    ### Make GnuPG gpg-agent ssh-agent thing work
+    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    set -gx GPG_TTY (tty)
+    gpg-connect-agent updatestartuptty /bye > /dev/null
 
-### Other Go stuff
-set -gx GO111MODULE on
+    ### Other Go stuff
+    set -gx GO111MODULE on
 
-### Fish greeting
-set -g fish_greeting ""
-
-### Start X at login
-# if status is-login
-#     if test -z "$DISPLAY" -a $XDG_VTNR = 1
-#         exec startx -- -keeptty
-#     end
-# end
+    ### Start X at login
+    if [ (tty) = "/dev/tty1" ]
+        exec startx -- -keeptty
+    end
+else
+    ### Fish greeting
+    set -g fish_greeting (tty)
+end
